fb01ec0c64e7328ef9df193138ae56d96f8c73e3
Leticia <contatoleticiadia@gmail.com>
2021-10-09 19:54:53 -0300

    feat: add create-class file

---
+++ create-class.php
@@ -0,0 +1,31 @@
+<?php
+
+use Alura\Pdo\Domain\Model\Student;
+use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
+use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
+
+require_once 'vendor/autoload.php';
+
+$connection = ConnectionCreator::createConnection();
+$studentRepository = new PdoStudentRepository($connection);
+
+
+$connection->beginTransaction();
+
+$aStudent = new Student(
+    null, 
+    'Rafael Garcia', 
+    new DateTimeImmutable('1999-10-23')
+);
+
+$studentRepository->save($aStudent);
+
+$anotherStudent = new Student(
+    null, 
+    'Fernanda Dias', 
+    new DateTimeImmutable('2000-12-21')
+);
+
+$studentRepository->save($anotherStudent);
+
+$connection->commit();
